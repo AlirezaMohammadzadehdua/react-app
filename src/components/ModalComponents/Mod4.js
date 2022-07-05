f98741e6c0a4aaaefcc7d107f7ef954e7ffe7893
Alireza Mohammadzadeh <peaky.jag@gmail.com>
2022-07-05 16:53:18 +0430

    Change Modal component

--- src/components/ModalComponents/Mod4.js
+++ src/components/ModalComponents/Mod4.js
@@ -24,14 +24,6 @@ export default function Modal() {
                                     <h3 className="text-3xl font-semibold">
                                         آشنایی با یادگیری ماشین
                                     </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                            ×
-                                        </span>
-                                    </button>
                                 </div>
                                 <div className="relative p-6 flex-auto">
                                     <p className="my-4 text-slate-500 text-lg leading-relaxed">
@@ -39,6 +31,13 @@ export default function Modal() {
                                     </p>
                                 </div>
                                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                    <button
+                                        className="border-[3px] border-blue-800 text-black hover:bg-blue-800 hover:text-white transform font-bold text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        بستن
+                                    </button>
                                 </div>
                             </div>
                         </div>
